9e8174316d480d9ad8c39064bb2fc3254189968c
Ayushi <115888395+Ayushi1245@users.noreply.github.com>
2026-07-24 00:01:50 +0530

    Time: 9 ms (11.69%), Space: 11.9 MB (29.99%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+        for(auto i:magazine){
+            if(mp.find(i)==mp.end()){
+                mp[i]=1;
+            }
+            else mp[i]++;
+        }
+        for(auto i:ransomNote){
+            if(mp.find(i)!=mp.end() && mp[i]>0)
+            mp[i]--;
+            else return false;
+        }
+        return true;
+    }
+};
